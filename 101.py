5e29baa1a0d594944e6605ebfe95852b8976497a
Laila Barradouane <LBarradouane@spartaglobal.com>
2020-02-05 11:10:59 +0000

    initial commit

---
+++ 101.py
@@ -0,0 +1,12 @@
+#exercise1
+
+name = 'Lana'
+Last_name = 'barradouane'
+age = 22
+eye_colour = 'brown'
+hair_colour = 'brown'
+
+name = input(f'Hello {name}!What new name would you like?')
+last_name = input('What name would you like?')
+
+print(f'Hello {name} {Last_name}! Welcome, your age is {age}, your eyes are {eye_colour}, and your hair colour is {hair_colour}')
